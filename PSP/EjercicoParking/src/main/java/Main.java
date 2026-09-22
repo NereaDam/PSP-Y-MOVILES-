59ed7ff4a27a3a2fba40f25e92fde9683f63d8d7
NereaDam <nereadam24@gmail.com>
2026-09-22 23:53:26 +0200

    Version con SEMAPHORE

--- PSP/EjercicoParking/src/main/java/Main.java
+++ PSP/EjercicoParking/src/main/java/Main.java
@@ -6,42 +6,28 @@ import java.util.concurrent.locks.ReentrantLock;
 
     void main()  {
 
-        ReentrantLock lock = new ReentrantLock();
-        Condition condition = lock.newCondition();
-        AtomicInteger plazasLibres =  new AtomicInteger(3);
+
+        Semaphore semaphore = new Semaphore(3);
 
         Runnable tareaCoche = () -> {
 
-            if (lock.tryLock()) {
-
-                while (plazasLibres.get() == 0) {
-                    try {
-                        condition.await();
-                        System.out.println("Esperando aparcamiento");
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                }
-                System.out.println("consigo plaza");
-                plazasLibres.decrementAndGet();
-                lock.unlock();
-                System.out.println("aparcado");
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
-                lock.lock();
-                plazasLibres.incrementAndGet();
-                condition.signal();
-                System.out.println("Me voy");
-
-                lock.unlock();
-
-
+            try {
+                semaphore.acquire();
+                System.out.println("Consigo plaza");
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
             }
+            System.out.println("aparcado");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            semaphore.release();
+            System.out.println("Me voy");
+
+
         };
 
         for (int i = 0; i < 5; i++) {
@@ -49,16 +35,7 @@ import java.util.concurrent.locks.ReentrantLock;
             coche.start();
 
         }
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
-        for (int i = 0; i <5; i++) {
-            Thread coche = new Thread(tareaCoche);
-            coche.start();
-        }
 
 
     }
